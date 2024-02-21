503d36123cb2e364f373d90dac74c622dcefdefb
julian piedrahita <julianpiedrahita@MacBook-Pro-de-julian.local>
2024-02-21 10:01:38 -0500

    start sonarqube

--- route53.tf
+++ route53.tf
@@ -1,7 +1,7 @@
 resource "aws_route53_record" "sonarQube" {
-  zone_id = aws_route53_zone.primary.zone_id
   name    = "www.sonarQubeCredicorp.com"
   type    = "A"
+  zone_id = 
   records = [aws_instance.Sonarqube.public_ip]
   ttl     = 300
   depends_on = [
